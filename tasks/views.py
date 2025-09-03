ce3318cb98aef6adbd0df0f9e260e6a1e9584aa7
Majkii1986 <majki.tvojarula@gmail.com>
2025-09-03 21:21:37 +0200

    views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,32 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import Task
-from .serializers import TaskSerializer, UserSerializer
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.contrib.auth.models import User
-from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()  # 👈 toto je dôležité, inak spadne na basename error
-    serializer_class = TaskSerializer    
-
-    def get_queryset(self):
-        return Task.objects.filter(owner=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-@api_view(['POST'])
-def register(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        user = serializer.save()
-        refresh = RefreshToken.for_user(user)
-        return Response({
-            'user_id': user.id,
-            'username': user.username,
-            'refresh': str(refresh),
-            'access': str(refresh.access_token)
-        })
-
-    return Response(serializer.errors, status=400)
+    serializer_class = TaskSerializer
