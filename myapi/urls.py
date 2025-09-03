234a08d428e3eb8bc30a6110ea980109ecaa0131
Majkii1986 <majki.tvojarula@gmail.com>
2025-09-03 21:23:04 +0200

    urls.py

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from tasks.views import TaskViewSet, register
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import routers
+from tasks.views import TaskViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/register/', register),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
